f1ebbe9b0df6303c2bc54aa6f2302df2b5d0c3aa
vishnu-ajit <heart.beats5717@gmail.com>
2013-10-16 16:30:27 +0530

    modified index.php..Now works fine within fb canvas.

--- index.php
+++ index.php
@@ -2,50 +2,46 @@
          <title>Example</title>                                                                                                                                                  
        </head>                                                                                                                                                                   
        <body>                                                                                                                                                                    
-         <div id="welcomeMessage"></div>                                                                                                                                         
+         <?php
+    require 'facebook.php';
 
-         <!-- Load the Facebook JavaScript SDK -->                                                                                                                               
-         <div id="fb-root"></div>                                                                                                                                                
-         <script>                                                                                                                                                                
-           window.fbAsyncInit = function() {                                                                                                                                     
-             FB.init({                                                                                                                                                           
-               appId      : 'APP_ID', // App ID                                                                                                                         
-               status     : true, // check login status                                                                                                                          
-               cookie     : true                                                                                                                                                 
-             });                                                                                                                                                                 
+    $app_id = '252235198257891';
+    $app_secret = 'bde69cb1d4b7857664728b52beca4d78';
+    $app_namespace = 'achillesgames';
+    $app_url = 'https://apps.facebook.com/' . $app_namespace . '/';
+    $scope = 'email,publish_actions';
 
-             // Check if the current user is logged in                                                                                                                           
-             // and has authorized the app                                                                                                                                       
-             FB.getLoginStatus(function(response) {                                                                                                                              
-               // Check the result of the user                                                                                                                                   
-               if(response && response.status == 'connected') {                                                                                                                  
-                 // The user is connected to Facebook                                                                                                                            
-                 // and has authorized the app.                                                                                                                                  
-                 // Now personalize the user experience                                                                                                                          
+    // Init the Facebook SDK
+    $facebook = new Facebook(array(
+         'appId'  => $app_id,
+         'secret' => $app_secret,
+));
 
-                 FB.api('/me', function(response) {                                                                                                                              
-                   var message = document.getElementById('welcomeMessage');                                                                                                      
-                   message.innerHTML = 'Hello, ' + response.first_name;                                                                                                          
-                 });                                                                                                                                                             
-               } else {                                                                                                                                                          
-                 // The user has not authenticated your app,                                                                                                                     
-                 // proceed with your normal (anonymous) flow.                                                                                                                   
-               }                                                                                                                                                                 
-             });                                                                                                                                                                 
-           };                                                                                                                                                                    
+// Get the current user
+$user = $facebook->getUser();
 
-           // Load the SDK Asynchronously
-           (function(d, s, id) {
-             var js, fjs = d.getElementsByTagName(s)[0];
-             if (d.getElementById(id)) return;
-             js = d.createElement(s); js.id = id;
-             js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=113869198637480";
-             fjs.parentNode.insertBefore(js, fjs);
-           }(document, 'script', 'facebook-jssdk'));
-         </script>
-		 testing fb canvas
-		 <?php
-		 echo "<fb:swf swfsrc=\"http://www.vishnuajit.com/gonein30seconds/GoneIn30Seconds.swf\" width=\"760\" height=\"710\" />";
-		 ?>
-		 
+// If the user has not installed the app, redirect them to the Login Dialog
+if (!$user) {
+        $loginUrl = $facebook->getLoginUrl(array(
+        'scope' => $scope,
+        'redirect_uri' => $app_url,
+        ));
+
+        print('<script> top.location.href=\'' . $loginUrl . '\'</script>');
+}
+?>
+<script>
+    var appId = '<?php echo $facebook->getAppID() ?>';
+    // Initialize the JS SDK
+    FB.init({
+        appId: appId,
+        cookie: true,
+    });
+
+    FB.getLoginStatus(function(response) {
+        uid = response.authResponse.userID ? response.authResponse.userID : null;
+    });
+</script>
+
+<embed src = "GoneIn30Seconds.swf" width=760 height = 600>
        </body>
